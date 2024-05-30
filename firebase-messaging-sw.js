41727c89f9cd833af5635cfa900ebc74a4f873ee
Dhrumil1411 <dhrumil1411@gmail.com>
2024-05-30 17:14:03 +0530

    update sw file

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,1 +1,25 @@
-console.log("In Firebase Messaging sw file");
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js";
+import {
+    getMessaging,
+    onMessage,
+  } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-messaging.js";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyB-r_eRpmJCAhjcwZiSKUy2QZlegPUCdNE",
+    authDomain: "chat-application-8e9ce.firebaseapp.com",
+    databaseURL:
+      "https://chat-application-8e9ce-default-rtdb.firebaseio.com",
+    projectId: "chat-application-8e9ce",
+    storageBucket: "chat-application-8e9ce.appspot.com",
+    messagingSenderId: "356463502797",
+    appId: "1:356463502797:web:642f8a38037347dafdd084",
+  };
+
+  // Initialize Firebase
+
+  const app = initializeApp(firebaseConfig);
+  const messaging = getMessaging(app);
+
+onMessage(messaging, (payload) => {
+    console.log('Message received. ', payload);
+  });
